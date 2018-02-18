1806b5436c89c0773e878c484fdb098e86665f00
temain <asv.artm@gmail.com>
2018-02-18 20:29:27 +0300

    Заявки, поставщики, склады

---
+++ VendingExpress.Web/Pages/Storages.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VendingExpress.Web.Pages
+{
+    public class StoragesModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
