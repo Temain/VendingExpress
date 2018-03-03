ceebbb8eb4dea7fcb4e60e4876a6ab8ed59d0595
temain <asv.artm@gmail.com>
2018-03-03 14:07:12 +0300

    Доработка: накладные, аренда площадей, заказ кофейных и снековых автоматов

---
+++ VendingExpress.Web/Pages/Automatons.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace VendingExpress.Pages
+{
+    [Authorize]
+    public class AutomatonsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
