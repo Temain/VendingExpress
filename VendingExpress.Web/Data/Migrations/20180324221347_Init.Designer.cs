44701af9f0457d65946b9046e1a6e68f92da0a74
temain <asv.artm@gmail.com>
2018-03-28 09:04:51 +0300

    Кнопки

--- VendingExpress.Web/Data/Migrations/20180324221347_Init.Designer.cs
+++ VendingExpress.Web/Data/Migrations/20180324221347_Init.Designer.cs
@@ -11,9 +11,10 @@ using VendingExpress.Data;
 namespace VendingExpress.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180324221347_Init")]
+    partial class Init
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- VendingExpress.Web/Pages/Automatons.cshtml.cs
+++ VendingExpress.Web/Pages/Automatons.cshtml.cs
@@ -2,13 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace VendingExpress.Pages
+namespace VendingExpress.Web.Pages
 {
-    [Authorize]
     public class AutomatonsModel : PageModel
     {
         public void OnGet()
